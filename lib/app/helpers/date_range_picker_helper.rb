ba7c124ef8c7d70b6e7d1d8f466e9afe1b0bc580
Eviathar Ben-lulu <eviatharbl@gmail.com>
2012-10-07 14:21:18 +0200

    m

--- lib/app/helpers/date_range_picker_helper.rb
+++ lib/app/helpers/date_range_picker_helper.rb
@@ -1,9 +1,9 @@
 module DateRangePicker
   module DateRangePickerHelper
 
-    puts "updated"
+    puts "no self"
 
-    def self.date_range_picker(attr = {})
+    def date_range_picker(attr = {})
       random_num = Random.rand(10000...99999);
       range_picker_id = attr[:range_picker_id] || "date-range-picker-"+random_num.to_s;
       range_picker_obj_name = attr[:js_obj_name] || "dataRangePicker"+random_num.to_s;
