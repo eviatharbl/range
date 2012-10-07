ee7851eeee5a02241c06e19c0af5387553c88034
Eviathar Ben-lulu <eviatharbl@gmail.com>
2012-10-07 14:43:08 +0200

    m

--- lib/date-range-picker.rb
+++ lib/date-range-picker.rb
@@ -2,15 +2,10 @@ require "app/helpers/date_range_picker_helper.rb"
 module DateRangePicker
   ActionController::Base.helper(DateRangePicker::DateRangePickerHelper)
 
-  module Rails
-    class Engine < ::Rails::Engine
-      # Rails, will you please look in our vendor? thx
-    end
-  end
+  #module Rails
+  #  class Engine < ::Rails::Engine
+  #    # Rails, will you please look in our vendor? thx
+  #  end
+  #end
 
-  base = File.join(File.dirname(__FILE__), '..')
-  styles = File.join(base, 'vendor', 'assets', 'stylesheets')
-  templates = File.join(base, 'templates')
-  ::Compass::Frameworks.register('bootstrap', :stylesheets_directory => styles, :templates_directory => templates)
-  puts "o-k"
 end
