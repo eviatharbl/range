bb9628c660737ee676371c3d6968c568fae21a3e
Eviathar Ben-lulu <eviatharbl@gmail.com>
2012-10-07 14:37:36 +0200

    m

--- lib/app/helpers/date_range_picker_helper.rb
+++ lib/app/helpers/date_range_picker_helper.rb
@@ -4,16 +4,16 @@ module DateRangePicker
     puts "no self"
 
     def date_range_picker(attr = {})
-      random_num = Random.rand(10000...99999);
-      range_picker_id = attr[:range_picker_id] || "date-range-picker-"+random_num.to_s;
-      range_picker_obj_name = attr[:js_obj_name] || "dataRangePicker"+random_num.to_s;
-      html = '<div class="' + (attr[:class] || '') + '" id="'+range_picker_id+'" ></div>';
+      random_num = Random.rand(10000...99999)
+      range_picker_id = attr[:range_picker_id] || "date-range-picker-"+random_num.to_s
+      range_picker_obj_name = attr[:js_obj_name] || "dataRangePicker"+random_num.to_s
+      html = '<div class="' + (attr[:class] || '') + '" id="'+range_picker_id+'" ></div>'
       html += javascript_tag(
           "$().ready(
           function () {
             #{range_picker_obj_name} = new dateRangePicker(
               '#{range_picker_id}',"+ActiveSupport::JSON.encode(attr)+")
-          });");
+          });")
 
       html.html_safe
     end
--- lib/date-range-picker.rb
+++ lib/date-range-picker.rb
@@ -8,5 +8,9 @@ module DateRangePicker
     end
   end
 
+  base = File.join(File.dirname(__FILE__), '..')
+  styles = File.join(base, 'vendor', 'assets', 'stylesheets')
+  templates = File.join(base, 'templates')
+  ::Compass::Frameworks.register('bootstrap', :stylesheets_directory => styles, :templates_directory => templates)
   puts "o-k"
 end
