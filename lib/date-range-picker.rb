6c1bdefd3aedb5c04c43ba8680fa1c0a5a2c39ef
Eviathar Ben-lulu <eviatharbl@gmail.com>
2012-10-07 14:30:34 +0200

    m

--- lib/date-range-picker.rb
+++ lib/date-range-picker.rb
@@ -1,5 +1,12 @@
 require "app/helpers/date_range_picker_helper.rb"
 module DateRangePicker
   ActionController::Base.helper(DateRangePicker::DateRangePickerHelper)
-  puts "ok"
+
+  module Rails
+    class Engine < ::Rails::Engine
+      # Rails, will you please look in our vendor? thx
+    end
+  end
+
+  puts "o-k"
 end
--- lib/date-range-picker/version.rb
+++ lib/date-range-picker/version.rb
@@ -1,3 +1,3 @@
 module DateRangePicker
-  VERSION = "0.0.8"
+  VERSION = "0.0.9"
 end
